0cf32233ef7cd445b5f06ff8fba2398b909f759d
romainGuarinoni <romain.guar91@gmail.com>
2021-07-17 01:01:31 +0200

    Updates

---
+++ precache-manifest.fcc068802d07e327b9353dde64cee4c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea6304be928fc6910a2fd7974e3dba2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bee944e207c38706d0e6",
+    "url": "./static/css/main.64f35fb0.chunk.css"
+  },
+  {
+    "revision": "ba707afcbb586c472730",
+    "url": "./static/js/2.3a50588e.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "./static/js/2.3a50588e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee944e207c38706d0e6",
+    "url": "./static/js/main.1a2d250a.chunk.js"
+  },
+  {
+    "revision": "f26be559a7416e3682b6",
+    "url": "./static/js/runtime-main.97703865.js"
+  }
+]);
